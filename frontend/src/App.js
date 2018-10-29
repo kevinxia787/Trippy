7a89206e5e75543075c3962c407ed43254a06c97
kevinxia787 <kevinxia787@gmail.com>
2018-10-29 16:52:20 -0400

    changes to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-<<<<<<< HEAD
   state = {
     data: null
   };
@@ -24,31 +23,14 @@ class App extends Component {
     return body;
   };
 
-=======
->>>>>>> a9619f55a7481cf863fa556bcd1693d445bc9a05
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
           <h1> Welcome to React + Express</h1>
         </header>
         <p className="App-intro">{this.state.data}</p>
-=======
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
->>>>>>> a9619f55a7481cf863fa556bcd1693d445bc9a05
       </div>
     );
   }
