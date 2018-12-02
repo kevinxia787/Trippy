be3502d9aece191f56925f40cbec6e662da76da6
kevinxia787 <kevinxia787@gmail.com>
2018-12-02 18:08:26 -0500

    converted category swiper menu to grid layout menu

--- frontend/CategoryMenu/CategoryMenu.js
+++ frontend/CategoryMenu/CategoryMenu.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { View, TouchableHighlight, Text, StyleSheet, Platform } from 'react-native';
-import { CardViewWithIcon } from 'react-native-simple-card-view'
+import { CardViewWithIcon, CardView } from 'react-native-simple-card-view'
 import { fetchLatLng } from '../services/FetchLatLng'
+import { Grid, Row, Col } from 'react-native-easy-grid';
 
 import Swiper from 'react-native-swiper';
 import IconFA from 'react-native-vector-icons/FontAwesome';
@@ -44,97 +45,126 @@ export default class CategoryMenu extends Component {
   render() {
     const {latLng } = this.state;
     return (
-      <Swiper>
-        <TouchableHighlight onPress={() => {
-          this.props.navigation.navigate('Cards', {
-            category: 'food',
-            location: latLng
-          })
-        }} style={styles.slide1}>
-          <View style={styles.center}>
-            <Material name="food" size={100} color="#fff"/>
-            <Text style={{color: '#FFF'}}>Food</Text>
+      <Grid>
+        <Row>
+          <View style={{width: '50%'}}>
+            <TouchableHighlight onPress={() => {
+              this.props.navigation.navigate('Cards', {
+                category: 'food',
+                location: latLng
+              })
+            }} style={styles.slide1}>
+              <View style={styles.center}>
+                <Material name="food" size={75} color="#fff"/>
+                <Text style={{color: '#FFF'}}>Food</Text>
+              </View>
+            </TouchableHighlight>
           </View>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => {
-          this.props.navigation.navigate('Cards', {
-            category: 'drinks',
-            location: latLng
-          })
-        }}
-          style={styles.slide2}>
-          <View style={styles.center}>
-            <Entypo name="drink" size={100} color="#fff"/>
-            <Text style={{color: '#FFF'}}>Drink</Text>
+          
+          <View style={{width: '50%'}}>
+            <TouchableHighlight onPress={() => {
+              this.props.navigation.navigate('Cards', {
+                category: 'drinks',
+                location: latLng
+              })
+            }}
+              style={styles.slide2}>
+              <View style={styles.center}>
+                <Entypo name="drink" size={75} color="#fff"/>
+                <Text style={{color: '#FFF'}}>Drink</Text>
+              </View>
+            </TouchableHighlight>
           </View>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => {
-          this.props.navigation.navigate('Cards', {
-            category: 'sights',
-            location: latLng
-          })
-        }} style={styles.slide3}>
-          <View style={styles.center}>
-            <Material name="binoculars" size={100} color="#fff"/>
-            <Text style={{color: '#FFF'}}>Sights</Text>
+        </Row>
+        <Row>
+           <View style={{width: '50%'}}>
+            <TouchableHighlight onPress={() => {
+              this.props.navigation.navigate('Cards', {
+                category: 'sights',
+                location: latLng
+              })
+            }} style={styles.slide3}>
+              <View style={styles.center}>
+                <Material name="binoculars" size={75} color="#fff"/>
+                <Text style={{color: '#FFF'}}>Sights</Text>
+              </View>
+            </TouchableHighlight >
           </View>
-        </TouchableHighlight >
-        <TouchableHighlight onPress={() => {
-          this.props.navigation.navigate('Cards', {
-            category: 'shops',
-            location: latLng
-          })
-        }} style={styles.slide4}>
-          <View style={styles.center}>
-            <Entypo name="shopping-cart" size={100} color="#fff"/>
-            <Text style={{color: '#FFF'}}>Shopping</Text>
+          <View style={{width: '50%'}}>
+            <TouchableHighlight onPress={() => {
+              this.props.navigation.navigate('Cards', {
+                category: 'shops',
+                location: latLng
+              })
+            }} style={styles.slide4}>
+              <View style={styles.center}>
+                <Entypo name="shopping-cart" size={75} color="#fff"/>
+                <Text style={{color: '#FFF'}}>Shopping</Text>
+              </View>
+            </TouchableHighlight>
           </View>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => {
-          this.props.navigation.navigate('Cards', {
-            category: 'outdoors',
-            location: latLng
-          })
-        }} style={styles.slide5}>
-          <View style={styles.center}>
-            <IconFA name="tree" size={100} color="#fff"/>
-            <Text style={{color: '#FFF'}}>Outdoors</Text>
+        </Row>
+        <Row>
+          <View style={{width: '50%'}}>
+            <TouchableHighlight onPress={() => {
+              this.props.navigation.navigate('Cards', {
+                category: 'outdoors',
+                location: latLng
+              })
+            }} style={styles.slide5}>
+              <View style={styles.center}>
+                <IconFA name="tree" size={75} color="#fff"/>
+                <Text style={{color: '#FFF'}}>Outdoors</Text>
+              </View>
+            </TouchableHighlight>
           </View>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => {
-          this.props.navigation.navigate('Cards', {
-            category: 'arts',
-            location: latLng
-          })
-        }} style={styles.slide6}>
-          <View style={styles.center}>
-            <IconFA name="paint-brush" size={100} color="#fff"/>
-            <Text style={{color: '#FFF'}}>Arts</Text>
+
+          <View style={{width: '50%'}}>
+            <TouchableHighlight onPress={() => {
+              this.props.navigation.navigate('Cards', {
+                category: 'arts',
+                location: latLng
+              })
+            }} style={styles.slide6}>
+              <View style={styles.center}>
+                <IconFA name="paint-brush" size={75} color="#fff"/>
+                <Text style={{color: '#FFF'}}>Arts</Text>
+              </View>
+            </TouchableHighlight>
           </View>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => {
-          this.props.navigation.navigate('Cards', {
-            category: 'trending',
-            location: latLng
-          })
-        }} style={styles.slide7} >
-          <View style={styles.center}>
-            <Feather name="trending-up" size={100} color="#fff"/>
-            <Text style={{color: '#FFF'}}>Trending</Text>
+          
+        </Row>
+        <Row>
+          <View style={{width: '50%'}}>
+            <TouchableHighlight onPress={() => {
+              this.props.navigation.navigate('Cards', {
+                category: 'topPicks',
+                location: latLng
+              })
+            }} style={styles.slide8} >
+              <View style={styles.center}>
+                <Entypo name="trophy" size={75} color="#fff"/>
+                <Text style={{color: '#FFF'}}>Top Picks</Text>
+              </View>
+            </TouchableHighlight>
           </View>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => {
-          this.props.navigation.navigate('Cards', {
-            category: 'topPicks',
-            location: latLng
-          })
-        }} style={styles.slide8} >
-          <View style={styles.center}>
-            <Entypo name="trophy" size={100} color="#fff"/>
-            <Text style={{color: '#FFF'}}>Top Picks</Text>
+        
+          <View style={{width: '50%'}}>
+            <TouchableHighlight onPress={() => {
+              this.props.navigation.navigate('Cards', {
+                category: 'trending',
+                location: latLng
+              })
+            }} style={styles.slide7} >
+              <View style={styles.center}>
+                <Feather name="trending-up" size={75} color="#fff"/>
+                <Text style={{color: '#FFF'}}>Trending</Text>
+              </View>
+            </TouchableHighlight>
           </View>
-        </TouchableHighlight>
-      </Swiper>
+        </Row>
+      </Grid>
+      
     )
   }
 }
@@ -145,7 +175,6 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
-    
   },  
   center: {
     flex: 1,
@@ -156,7 +185,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#D63230'
+    backgroundColor: '#D63230',
   },
   slide2: {
     flex: 1, 
