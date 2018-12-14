398e84959d54c5fac30a93ce09c86aa6fe35ee7d
kevinxia787 <kevinxia787@gmail.com>
2018-12-14 12:02:23 -0500

    couldnt add gaps to categories

--- frontend/CategoryMenu/CategoryMenu.js
+++ frontend/CategoryMenu/CategoryMenu.js
@@ -44,62 +44,54 @@ export default class CategoryMenu extends Component {
   render() {
     const {latLng } = this.state;
     return (
-      <Grid>
-        <Row>
-          <View style={{width: '50%'}}>
-            <TouchableHighlight style={{borderRadius: 10}} onPress={() => {
-                this.props.navigation.navigate('Cards', {
-                  category: 'food',
-                  startLocation: latLng,
-                  startAddress: this.props.navigation.state.params.location,
-                })
-              }}>
-              <ImageBackground source={require('../assets/images/food.jpg')} style={{tintColor: 'black', borderRadius: 10, width: '100%', height: '100%'}}>
-                <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
-                  <Text style={styles.text}>Food</Text>
-                </View>
-              </ImageBackground>
-            </TouchableHighlight>
-            
-            
-          </View>
-          
-          <View style={{width: '50%'}}>
-            <TouchableHighlight onPress={() => {
+      <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
+        <View style={{width: '50%', height: '25%', borderRadius: 5}}>
+          <TouchableHighlight style={{borderRadius: 10}} onPress={() => {
+              this.props.navigation.navigate('Cards', {
+                category: 'food',
+                startLocation: latLng,
+                startAddress: this.props.navigation.state.params.location,
+              })
+            }}>
+            <ImageBackground source={require('../assets/images/food.jpg')} style={{tintColor: 'black', borderRadius: 10, width: '100%', height: '100%'}}>
+              <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
+                <Text style={styles.text}>Food</Text>
+              </View>
+            </ImageBackground>
+          </TouchableHighlight>
+        </View>
+        <View style={{width: '50%', height: '25%'}}>
+          <TouchableHighlight style={{borderRadius: 10}} onPress={() => {
               this.props.navigation.navigate('Cards', {
                 category: 'drinks',
                 startLocation: latLng,
                 startAddress: this.props.navigation.state.params.location,
               })
-            }}
-              style={styles.slide2}>
-              <ImageBackground source={require('../assets/images/drinks.jpg')} style={{width: '100%', height: '100%'}}>
-                <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
-                  <Text style={styles.text}>Drinks</Text>
-                </View>
-              </ImageBackground>
-            </TouchableHighlight>
-          </View>
-        </Row>
-        <Row>
-           <View style={{width: '50%'}}>
-            <TouchableHighlight onPress={() => {
+            }}>
+            <ImageBackground source={require('../assets/images/drinks.jpg')} style={{tintColor: 'black', borderRadius: 10, width: '100%', height: '100%'}}>
+              <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
+                <Text style={styles.text}>Drinks</Text>
+              </View>
+            </ImageBackground>
+          </TouchableHighlight>
+        </View>
+        <View style={{width: '50%', height: '25%'}}>
+          <TouchableHighlight onPress={() => {
               this.props.navigation.navigate('Cards', {
                 category: 'sights',
                 startLocation: latLng,
                 startAddress: this.props.navigation.state.params.location,
               })
             }} style={styles.slide3}>
-              <ImageBackground source={require('../assets/images/sights.jpg')} style={{width: '100%', height: '100%'}}>
-                <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
-                  <Text style={styles.text}>Sights</Text>
-                </View>
-              </ImageBackground>
-
-            </TouchableHighlight >
-          </View>
-          <View style={{width: '50%'}}>
-            <TouchableHighlight onPress={() => {
+            <ImageBackground source={require('../assets/images/sights.jpg')} style={{width: '100%', height: '100%'}}>
+              <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
+                <Text style={styles.text}>Sights</Text>
+              </View>
+            </ImageBackground>
+          </TouchableHighlight >
+        </View>
+        <View style={{width: '50%', height: '25%'}}>
+         <TouchableHighlight onPress={() => {
               this.props.navigation.navigate('Cards', {
                 category: 'shops',
                 startLocation: latLng,
@@ -112,11 +104,9 @@ export default class CategoryMenu extends Component {
                 </View>
               </ImageBackground>
             </TouchableHighlight>
-          </View>
-        </Row>
-        <Row>
-          <View style={{width: '50%'}}>
-            <TouchableHighlight onPress={() => {
+        </View>
+        <View style={{width: '50%', height: '25%'}}>
+          <TouchableHighlight onPress={() => {
               this.props.navigation.navigate('Cards', {
                 category: 'outdoors',
                 startLocation: latLng,
@@ -128,11 +118,10 @@ export default class CategoryMenu extends Component {
                   <Text style={styles.text}>Outdoors</Text>
                 </View>
               </ImageBackground>
-            </TouchableHighlight>
-          </View>
-
-          <View style={{width: '50%'}}>
-            <TouchableHighlight onPress={() => {
+          </TouchableHighlight>
+        </View>
+        <View style={{width: '50%', height: '25%'}}>
+           <TouchableHighlight onPress={() => {
               this.props.navigation.navigate('Cards', {
                 category: 'arts',
                 startLocation: latLng,
@@ -145,12 +134,9 @@ export default class CategoryMenu extends Component {
                 </View> 
               </ImageBackground>
             </TouchableHighlight>
-          </View>
-          
-        </Row>
-        <Row>
-          <View style={{width: '50%'}}>
-            <TouchableHighlight onPress={() => {
+        </View>
+        <View style={{width: '50%', height: '25%'}}>
+          <TouchableHighlight onPress={() => {
               this.props.navigation.navigate('Cards', {
                 category: 'topPicks',
                 startLocation: latLng,
@@ -163,10 +149,9 @@ export default class CategoryMenu extends Component {
                 </View>
               </ImageBackground>
             </TouchableHighlight>
-          </View>
-        
-          <View style={{width: '50%'}}>
-            <TouchableHighlight onPress={() => {
+        </View>
+        <View style={{width: '50%', height: '25%'}}>
+          <TouchableHighlight onPress={() => {
               this.props.navigation.navigate('Cards', {
                 category: 'trending',
                 startLocation: latLng,
@@ -178,10 +163,135 @@ export default class CategoryMenu extends Component {
                   <Text style={styles.text}>Trending</Text>
                 </View>
               </ImageBackground>
-            </TouchableHighlight>
-          </View>
-        </Row>
-      </Grid>
+          </TouchableHighlight>
+        </View>
+      </View>
+     
+      // <Grid>
+      //   <Row>
+      //     <View style={{width: '50%'}}>
+      //       <TouchableHighlight style={{borderRadius: 10}} onPress={() => {
+      //           this.props.navigation.navigate('Cards', {
+      //             category: 'food',
+      //             startLocation: latLng,
+      //             startAddress: this.props.navigation.state.params.location,
+      //           })
+      //         }}>
+      //         <ImageBackground source={require('../assets/images/food.jpg')} style={{tintColor: 'black', borderRadius: 10, width: '100%', height: '100%'}}>
+      //           <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
+      //             <Text style={styles.text}>Food</Text>
+      //           </View>
+      //         </ImageBackground>
+      //       </TouchableHighlight>
+            
+            
+      //     </View>
+          
+      //     <View style={{width: '50%'}}>
+      //       <TouchableHighlight onPress={() => {
+      //         this.props.navigation.navigate('Cards', {
+      //           category: 'drinks',
+      //           startLocation: latLng,
+      //           startAddress: this.props.navigation.state.params.location,
+      //         })
+      //       }}
+      //         style={styles.slide2}>
+      //         <ImageBackground source={require('../assets/images/drinks.jpg')} style={{width: '100%', height: '100%'}}>
+      //           <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
+      //             <Text style={styles.text}>Drinks</Text>
+      //           </View>
+      //         </ImageBackground>
+      //       </TouchableHighlight>
+      //     </View>
+      //   </Row>
+      //   <Row>
+      //      <View style={{width: '50%'}}>
+            
+      //     </View>
+      //     <View style={{width: '50%'}}>
+      //       <TouchableHighlight onPress={() => {
+      //         this.props.navigation.navigate('Cards', {
+      //           category: 'shops',
+      //           startLocation: latLng,
+      //           startAddress: this.props.navigation.state.params.location,
+      //         })
+      //       }} style={styles.slide4}>
+      //          <ImageBackground source={require('../assets/images/shopping.jpg')} style={{width: '100%', height: '100%'}}>
+      //           <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
+      //             <Text style={styles.text}>Shopping</Text>
+      //           </View>
+      //         </ImageBackground>
+      //       </TouchableHighlight>
+      //     </View>
+      //   </Row>
+      //   <Row>
+      //     <View style={{width: '50%'}}>
+            // <TouchableHighlight onPress={() => {
+            //   this.props.navigation.navigate('Cards', {
+            //     category: 'outdoors',
+            //     startLocation: latLng,
+            //     startAddress: this.props.navigation.state.params.location,
+            //   })
+            // }} style={styles.slide5}>
+            //    <ImageBackground source={require('../assets/images/outdoor.jpg')} style={{width: '100%', height: '100%'}}>
+            //     <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
+            //       <Text style={styles.text}>Outdoors</Text>
+            //     </View>
+            //   </ImageBackground>
+            // </TouchableHighlight>
+      //     </View>
+
+      //     <View style={{width: '50%'}}>
+            // <TouchableHighlight onPress={() => {
+            //   this.props.navigation.navigate('Cards', {
+            //     category: 'arts',
+            //     startLocation: latLng,
+            //     startAddress: this.props.navigation.state.params.location,
+            //   })
+            // }} style={styles.slide6}>
+            //    <ImageBackground source={require('../assets/images/arts.jpg')} style={{width: '100%', height: '100%'}}>
+            //     <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
+            //       <Text style={styles.text}>Arts</Text>
+            //     </View> 
+            //   </ImageBackground>
+            // </TouchableHighlight>
+      //     </View>
+          
+      //   </Row>
+      //   <Row>
+      //     <View style={{width: '50%'}}>
+            // <TouchableHighlight onPress={() => {
+            //   this.props.navigation.navigate('Cards', {
+            //     category: 'topPicks',
+            //     startLocation: latLng,
+            //     startAddress: this.props.navigation.state.params.location,
+            //   })
+            // }} style={styles.slide8} >
+            //    <ImageBackground source={require('../assets/images/toppicks.jpg')} style={{width: '100%', height: '100%'}}>
+            //     <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
+            //       <Text style={styles.text}>Top Picks</Text>
+            //     </View>
+            //   </ImageBackground>
+            // </TouchableHighlight>
+      //     </View>
+        
+      //     <View style={{width: '50%'}}>
+            // <TouchableHighlight onPress={() => {
+            //   this.props.navigation.navigate('Cards', {
+            //     category: 'trending',
+            //     startLocation: latLng,
+            //     startAddress: this.props.navigation.state.params.location,
+            //   })
+            // }} style={styles.slide7} >
+            //    <ImageBackground source={require('../assets/images/trending.jpg')} style={{width: '100%', height: '100%'}}>
+            //     <View style={{backgroundColor: 'rgba(0,0,0,0.4)', flex: 1}} > 
+            //       <Text style={styles.text}>Trending</Text>
+            //     </View>
+            //   </ImageBackground>
+            // </TouchableHighlight>
+      //     </View>
+      //   </Row>
+      // </Grid>
       
     )
   }
